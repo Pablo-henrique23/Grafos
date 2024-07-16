f1d146a0053bbd4661ed02f3c976c83b633809ac
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2024-07-16 17:48:27 -0300

    Início

---
+++ Graph-Basis/source/Graph.cpp
@@ -0,0 +1,42 @@
+#include "../include/Graph.hpp"
+
+Graph::Graph(std::ifstream& instance)
+{
+}
+
+Graph::Graph()
+{
+}
+
+Graph::~Graph()
+{
+}
+
+void Graph::remove_node(size_t node_position)
+{
+}
+
+void Graph::remove_edge(size_t node_position_1, size_t node_position_2)
+{
+}
+
+void Graph::add_node(size_t node_id, float weight)
+{
+}
+
+void Graph::add_edge(size_t node_id_1, size_t node_id_2, float weight)
+{
+}
+
+void Graph::print_graph()
+{
+}
+
+void Graph::print_graph(std::ofstream& output_file)
+{
+}
+
+int Graph::conected(size_t node_id_1, size_t node_id_2)
+{
+    return 0;
+}
